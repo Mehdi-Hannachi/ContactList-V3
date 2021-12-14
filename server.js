22d6ef7119984552d781d9eab8427377dc9b004b
Firas <mehdihannachi02@gmail.com>
2021-12-14 15:49:03 +0100

    Create data base connection using mongoose (mongoDB Atlas)

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("dotenv").config({ path: "./config/.env" });
 
 const express = require("express");
+const connectDB = require("./config/connectDB");
 
 const app = express();
 
 app.use(express.json());
+
+connectDB();
 
 app.listen(process.env.PORT, (err) => {
   err
